f497b77218ef82433f71b7386b6085a1f50d526e
Pushkinmazila2 <zavalny199@yandex.com>
2026-04-23 00:58:30 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,7 +39,7 @@ fi
 ss-server -v -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u &
 SS_SERVER_PID=$!
 
-ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u &
+ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -v -u &
 SS_LOCAL_PID=$!
 
 wait
