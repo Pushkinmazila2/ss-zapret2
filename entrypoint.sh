9adb67b1fca5750896494143cec23218b66316ee
Pushkinmazila2 <zavalny199@yandex.com>
2026-04-23 01:11:03 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,10 +36,21 @@ if [ "${SS_VERBOSE:-1}" = "0" ]; then
   exec >/dev/null 2>&1
 fi
 
-ss-server -v -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u 2>&1 | sed 's/^/[SS-SERVER] /' &
+# Создаем named pipes для логов
+mkfifo /tmp/ss-server.log /tmp/ss-local.log
+
+# Запускаем процессы с перенаправлением в pipes
+ss-server -v -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u >/tmp/ss-server.log 2>&1 &
 SS_SERVER_PID=$!
 
-ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -v -u 2>&1 | sed 's/^/[SS-LOCAL] /' &
+ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -v -u >/tmp/ss-local.log 2>&1 &
 SS_LOCAL_PID=$!
+
+# Читаем из pipes и добавляем префиксы
+sed 's/^/[SS-SERVER] /' </tmp/ss-server.log &
+TAIL_SERVER_PID=$!
+
+sed 's/^/[SS-LOCAL] /' </tmp/ss-local.log &
+TAIL_LOCAL_PID=$!
 
 wait
