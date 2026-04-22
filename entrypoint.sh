b926485de2f237fd84753685fb982ade20250fcf
Pushkinmazila2 <zavalny199@yandex.com>
2026-04-23 01:05:12 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,10 +36,10 @@ if [ "${SS_VERBOSE:-1}" = "0" ]; then
   exec >/dev/null 2>&1
 fi
 
-ss-server -v -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u &
+ss-server -v -s 0.0.0.0 -p "${SS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -u 2>&1 | sed 's/^/[SS-SERVER] /' &
 SS_SERVER_PID=$!
 
-ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -v -u &
+ss-local -b 0.0.0.0 -s 127.0.0.1 -p "${SS_PORT}" -l "${SOCKS_PORT}" -k "${SS_PASSWORD}" -m "${SS_ENCRYPT_METHOD}" -t "${SS_TIMEOUT}" -v -u 2>&1 | sed 's/^/[SS-LOCAL] /' &
 SS_LOCAL_PID=$!
 
 wait
